fca0b8ebcddfa4de27c0bc171cbd4ebc64261eec
unknown <christian@linkernetworks.com>
2022-03-10 19:24:48 +0800

    fix writing error

--- coco_to_yolo.py
+++ coco_to_yolo.py
@@ -8,23 +8,33 @@ import  numpy as np
 import aiofiles
 import asyncio
 from threading import Thread
+import shutil
+import time
 
+class MyThread(Thread):
+    def __init__(self, data,  name):
+        Thread.__init__(self)
+        self.name = name
+        self.data = data
+        self.delay = 0.1
 
-def write_file(line, dest):
-    with open(dest, 'w+') as file:
-        file.write(('%g ' * len(line)).rstrip() % line + '\n')
+    def run(self):
+        with open(self.name, 'a+') as file:
+            file.write(self.data)
+            file.flush()
+            time.sleep(self.delay)
+
         
-def write_file_list(filenames, dest):
-    with open(dest, 'w+') as file:
-        for line in filenames:
-            file.write('%s' % line + '\n')
-        
-def convert_coco_json(json_dir='../coco/annotations/',output_dir='../coco/labels/',
-       image_dir="../coco/data", text_dir='../coco/', batch_size=256, use_segments=False):
+def convert_coco_json(json_dir='./datasets/coco/annotations/',output_dir='./datasets/coco/labels/',
+       image_dir="./datasets/coco/data", text_dir='./datasets/coco/', batch_size=8, use_segments=False):
     if json_dir == None or output_dir ==None or text_dir==None or image_dir==None:
         print("Directories can't be none")
         return
+    
     os.makedirs(output_dir,exist_ok=True)
+    shutil.rmtree(output_dir)
+    os.makedirs(output_dir,exist_ok=True)
+    
     jsons = glob.glob(json_dir + '*.json')
 
     fn = Path(output_dir)
@@ -64,31 +74,34 @@ def convert_coco_json(json_dir='../coco/annotations/',output_dir='../coco/labels
             # Write
             if box[2] > 0 and box[3] > 0:  # if w > 0 and h > 0
                 line = x['category_id'], *(s if use_segments else box)  # cls, box or segments
-                
                 #th = Thread(target=write_file, args=(line,(fn / f).with_suffix('.txt')))
-                task = Thread(target=write_file,args = (line,(fn / f).with_suffix('.txt')))
-                task.start()
+                new_line = "".join(('%g ' * len(line)).rstrip() % line) +"\n"
+                task = MyThread(new_line,(fn / f).with_suffix('.txt'))
                 tasks.append(task)
-            
-            if i == batch_size-1:
-                [t.join() for t in tasks]
-                tasks = []
+                task.start()
+
             
             i = (i + 1 ) % batch_size
-        filenames = list(set(filenames))          
-        task = Thread(target=write_file_list, args=(filenames, os.path.join(text_dir, sub_type+".txt")))
-        task.start()
-        tasks.append(task)
-        if tasks:
-            [t.join() for t in tasks]
-        
+        filenames = list(set(filenames))
+        for file in filenames:          
+            task = MyThread(file, os.path.join(text_dir, sub_type+".txt"))
+            tasks.append(task)
+            task.start()
+        # wait for threads to finish
+        while True:
+            if any(task.is_alive() for task in tasks):
+                time.sleep(0.1)
+            else:
+                print("All done.")
+                break
+                
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--json-dir', type=str, default=None, help='jsons folder path')
-    parser.add_argument('--output-dir', type=str, default=None, help='labels output folder path')
-    parser.add_argument('--image-dir', type=str, default=None, help='labels output folder path')
-    parser.add_argument('--text-dir', type=str, default=None, help='labels output folder path')
+    parser.add_argument('--json-dir', type=str, default='.\\datasets\\coco\\annotations\\', help='jsons folder path')
+    parser.add_argument('--output-dir', type=str, default='.\\datasets\\coco\\labels\\', help='labels output folder path')
+    parser.add_argument('--image-dir', type=str, default='.\\datasets\\coco\\data\\', help='labels output folder path')
+    parser.add_argument('--text-dir', type=str, default='.\\datasets\\coco\\', help='labels output folder path')
     args = parser.parse_args()
     
     convert_coco_json(args.json_dir, args.output_dir, args.image_dir,args.text_dir)
